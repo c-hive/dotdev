d05189401629373bfdf5671ba8285d402e54ea0d
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-05-19 10:45:22 +0200

    Remove _app.js.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import React from "react";
+import Head from "next/head";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "../src/common/GlobalStyle/GlobalStyle";
 import App from "../src/components/App/App";
 import { ThemeProvider } from "../src/contexts/Theme";
 import { ConfigProvider } from "../src/contexts/Config";
+// The CI would complain about the missing config file if the rule was not ignored.
+// Reason: at build-time, the config comes from an environment variable so it's not committed directly to the repository.
+// eslint-disable-next-line import/no-unresolved
+import config from "../config/config.yml";
 
 const indexPage = () => (
-  <ThemeProvider>
-    <Normalize />
-    <GlobalStyle />
-    <ConfigProvider>
-      <App />
-    </ConfigProvider>
-  </ThemeProvider>
+  <>
+    <Head>
+      <title>{config.name}</title>
+    </Head>
+    <ThemeProvider>
+      <Normalize />
+      <GlobalStyle />
+      <ConfigProvider>
+        <App />
+      </ConfigProvider>
+    </ThemeProvider>
+  </>
 );
 
 export default indexPage;
