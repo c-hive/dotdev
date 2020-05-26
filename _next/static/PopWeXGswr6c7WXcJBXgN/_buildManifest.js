0d89ea75876625de0238af92c32128bc8132bc7c
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-05-26 16:55:11 +0200

    Deploying to gh-pages

--- _next/static/PopWeXGswr6c7WXcJBXgN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F451823fb.4cfbc91cf173726a200d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
